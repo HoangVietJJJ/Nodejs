562eba92853439da4fdbbadb1570cc18a3270c73
HoangViet <hoangphamhl000@gmail.com>
2023-07-10 21:00:39 +0700

    5

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -32,9 +32,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDservice.getAllUser();
-    console.log('----------------------------')
-    console.log(data)
-    console.log('----------------------------')
     return res.render('displayCRUD.ejs', {
         dataTable: data
     })
@@ -64,6 +61,17 @@ let putCRUD = async (req, res) => {
     })
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDservice.deleteUserById(id);
+        return res.send('Delete done1')
+    }
+    else {
+        return res.send('User not found!')
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
@@ -72,4 +80,5 @@ module.exports = {
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
     putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -9,10 +9,13 @@ let initWebRoutes = (app) => {
     router.get('/', homeControllers.getHomePage);
     router.get('/about', homeControllers.getAboutPage);
     router.get('/crud', homeControllers.getCRUD);
+
     router.post('/post-crud', homeControllers.postCRUD);
     router.get('/get-crud', homeControllers.displayGetCRUD);
     router.get('/edit-crud', homeControllers.getEditCRUD);
+
     router.post('/put-crud', homeControllers.putCRUD);
+    router.get('/delete-crud', homeControllers.deleteCRUD);
 
 
     return app.use("/", router);
